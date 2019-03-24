270810cf8f9c9bd500a8d6362b289e3ae9ccde6e
daniela <matias.benedetto@gmail.com>
2019-03-24 17:19:48 -0300

    adding tests for Paper.addPoint()

---
+++ components/canvas/Paper.test.js
@@ -0,0 +1,78 @@
+import Paper from './Paper'
+
+const pointToAdd = {
+    x:300,
+    y:400,
+    size: 11,
+    fill: 'red',
+    print: true
+}
+
+const parallelogramPoints = [
+    {
+        x:100,
+        y:300,
+        size: 11,
+        fill: 'red',
+        print: true
+    },
+    {
+        x:200,
+        y:400,
+        size: 11,
+        fill: 'red',
+        print: true
+    },
+    {
+        x:300,
+        y:400,
+        size: 11,
+        fill: 'red',
+        print: true
+    },
+    {
+        x:200,
+        y:300,
+        size: 11,
+        print: false
+    }
+]
+
+describe ('Add points in paper to create a parallelogram', () => {
+    it('adds a point to an empty matrix', () => {
+        const paper = new Paper()
+        const points = [[]]
+        const data = paper.addPoint(points, pointToAdd)
+        expect(data).toEqual([[pointToAdd]])
+    })
+
+    it('adds a point to a matrix with 2 points', () => {
+        const paper = new Paper()
+        const points = [[
+            {
+                x:100,
+                y:300,
+                size: 11,
+                fill: 'red',
+                print: true
+            },
+            {
+                x:200,
+                y:400,
+                size: 11,
+                fill: 'red',
+                print: true
+            }
+        ]]
+        const data = paper.addPoint(points, pointToAdd)
+        expect(data).toEqual([parallelogramPoints])
+    })
+
+    it('adds a point to a matrix with a complete group of 4 points', () => {
+        const paper = new Paper()
+        const points = [parallelogramPoints]
+        const data = paper.addPoint(points, pointToAdd)
+        expect(data).toEqual([parallelogramPoints, [pointToAdd]])
+    })
+
+})
