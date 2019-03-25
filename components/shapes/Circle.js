502a0a6b50b86295d12d5e9e4a961ea0adfc08fc
daniela <matias.benedetto@gmail.com>
2019-03-25 20:52:09 -0300

    restoring classes in components

--- components/shapes/Circle.js
+++ components/shapes/Circle.js
@@ -1,8 +1,13 @@
 import PropTypes from 'prop-types'
 
-export default function Circle (props) {
-  function handleMouseDown (event) {
-    const { id, parentId, draggable, toggleIsDraggingPoint, updateLastSelectedPoint } = props
+export default class Circle extends React.Component{
+  constructor (props) {
+    super(props)
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+  }
+
+  handleMouseDown (event) {
+    const { id, parentId, draggable, toggleIsDraggingPoint, updateLastSelectedPoint } = this.props
     if (draggable) {
       toggleIsDraggingPoint()
       updateLastSelectedPoint(id, parentId)
@@ -11,37 +16,39 @@ export default function Circle (props) {
     }
   }
 
-  let { x, y, size, fill, stroke, draggable, centered } = props
-  if (centered) {
-    x = x - size / 2
-    y = y - size / 2
+  render(){
+    let { x, y, size, fill, stroke, draggable, centered } = this.props
+    if (centered) {
+      x = x - size / 2
+      y = y - size / 2
+    }
+    return (
+      <React.Fragment>
+        <style jsx>{`
+                        .circle {
+                            cursor: ${draggable ? 'grab' : 'auto'};
+                            pointer-events: ${draggable ? 'auto' : 'none'};
+                        }
+                        text{
+                            font-family: Sans-Serif;
+                            font-size: 10px;
+                            pointer-events: none;
+                            user-select: none;
+                        }
+                `}</style>
+        <circle
+          cx={x}
+          cy={y}
+          r={size}
+          fill={fill}
+          stroke={stroke}
+          className='circle'
+          onMouseDown={this.handleMouseDown}
+        />
+        <text x={x} y={y} fill={fill} transform={`translate( ${size + 10} )`}>{ `x:${~~x}, y:${~~y} ` }</text>
+      </React.Fragment>
+    )
   }
-  return (
-    <React.Fragment>
-      <style jsx>{`
-                      .circle {
-                          cursor: ${draggable ? 'grab' : 'auto'};
-                          pointer-events: ${draggable ? 'auto' : 'none'};
-                      }
-                      text{
-                          font-family: Sans-Serif;
-                          font-size: 10px;
-                          pointer-events: none;
-                          user-select: none;
-                      }
-              `}</style>
-      <circle
-        cx={x}
-        cy={y}
-        r={size}
-        fill={fill}
-        stroke={stroke}
-        className='circle'
-        onMouseDown={handleMouseDown}
-      />
-      <text x={x} y={y} fill={fill} transform={`translate( ${size + 10} )`}>{ `x:${~~x}, y:${~~y} ` }</text>
-    </React.Fragment>
-  )
 }
 
 Circle.propTypes = {
--- components/shapes/Parallelogram.js
+++ components/shapes/Parallelogram.js
@@ -1,8 +1,9 @@
 import Circle from './Circle'
 import PropTypes from 'prop-types'
 
-export default function Parallelogram (props) {
-  function getPolygonCoordsString (points) {
+export default class Parallelogram extends React.Component {
+
+  getPolygonCoordsString (points) {
     let coordPoints = ''
     points.forEach(point => {
       coordPoints += `${point.x}, ${point.y} `
@@ -10,16 +11,16 @@ export default function Parallelogram (props) {
     return coordPoints
   }
 
-  function getCenter (points) {
+  getCenter (points) {
     const x = (points[0].x + points[2].x) / 2
     const y = (points[0].y + points[2].y) / 2
     return { x, y }
   }
 
-  function getCenteredCircle (points, area) {
+  getCenteredCircle (points, area) {
     if (points.length === 4) {
-      const center = getCenter(points)
-      const radius = getRadiusFromArea(area)
+      const center = this.getCenter(points)
+      const radius = this.getRadiusFromArea(area)
       const x = center.x + radius / 2
       const y = center.y + radius / 2
       return (<Circle x={x} y={y} fill='transparent' stroke='yellow' size={radius} stroke-width='3' draggable={false} centered />)
@@ -27,64 +28,66 @@ export default function Parallelogram (props) {
     return null
   }
 
-  function getDistance (point1, point2) {
+  getDistance (point1, point2) {
     const a = point1.x - point2.x
     const b = point1.y - point2.y
     return Math.sqrt(a * a + b * b)
   }
 
-  function getArea (points) {
+  getArea (points) {
     if (points.length === 4) {
-      const b = getDistance(points[0], points[1])
-      const h = getDistance(points[1], points[2])
+      const b = this.getDistance(points[0], points[1])
+      const h = this.getDistance(points[1], points[2])
       return ~~(b * h)
     } else {
       return undefined
     }
   }
 
-  function getRadiusFromArea (area) {
+  getRadiusFromArea (area) {
     const radius = Math.sqrt(area / Math.PI)
     return radius
   }
 
-  const { id, points, toggleIsDraggingPoint, updateLastSelectedPoint } = props
-  const coords = getPolygonCoordsString(points)
-  const area = getArea(points)
-  const centeredCircle = getCenteredCircle(points, area)
-  return (
-    <React.Fragment>
-      <style jsx>{`
-                  polygon{
-                      pointer-events: none;
-                  }
-                  text{
-                      font-family: Sans-Serif;
-                      font-size: 10px;
-                      user-select: none;
-                  }
-              `}</style>
-      <polygon points={coords} fill='transparent' stroke='blue' />
-      { points.map((point, i) => (
-        point.print &&
-        <React.Fragment key={i}>
-          <Circle
-            {...point}
-            id={i}
-            parentId={id}
-            toggleIsDraggingPoint={toggleIsDraggingPoint}
-            updateLastSelectedPoint={updateLastSelectedPoint}
-            draggable
-          />
-          { (i === 0 && points.length === 4) &&
-          <text x={point.x} y={point.y} fill='blue' transform={`translate( -10, -20 )`} >Area: { area }</text>
-          }
-        </React.Fragment>
-      )) };
+  render(){
+    const { id, points, toggleIsDraggingPoint, updateLastSelectedPoint } = this.props
+    const coords = this.getPolygonCoordsString(points)
+    const area = this.getArea(points)
+    const centeredCircle = this.getCenteredCircle(points, area)
+    return (
+      <React.Fragment>
+        <style jsx>{`
+                    polygon{
+                        pointer-events: none;
+                    }
+                    text{
+                        font-family: Sans-Serif;
+                        font-size: 10px;
+                        user-select: none;
+                    }
+                `}</style>
+        <polygon points={coords} fill='transparent' stroke='blue' />
+        { points.map((point, i) => (
+          point.print &&
+          <React.Fragment key={i}>
+            <Circle
+              {...point}
+              id={i}
+              parentId={id}
+              toggleIsDraggingPoint={toggleIsDraggingPoint}
+              updateLastSelectedPoint={updateLastSelectedPoint}
+              draggable
+            />
+            { (i === 0 && points.length === 4) &&
+            <text x={point.x} y={point.y} fill='blue' transform={`translate( -10, -20 )`} >Area: { area }</text>
+            }
+          </React.Fragment>
+        )) };
 
-      { centeredCircle }
-    </React.Fragment>
-  )
+        { centeredCircle }
+      </React.Fragment>
+    )
+  }
 }
 
 Parallelogram.propTypes = {
